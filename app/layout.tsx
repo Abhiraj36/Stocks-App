b0c8c51490bf6622d26caa1e78b7af76dcd66375
Abhiraj36 <emailabhiraj061@gmail.com>
2025-10-13 12:35:31 +0530

    Started working on Layout and Headers, shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Investmate",
+  title: "Signalist",
   description: "Track real-time stock prices, get personlized alerts and explore detailed company insights." ,
 };
 
